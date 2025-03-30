644d5e5e9254e35034aa88bd85961228faed884b
Youssef Oubrik <yo.oubrik@gmail.com>
2025-03-30 21:39:50 +0000

    feat(auth): add unauthorized access page

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -3,15 +3,17 @@
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
-export default function NotFound() {
+export default function Unauthorized() {
   const router = useRouter();
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
-      <h1 className="text-9xl font-bold text-primary-dark/80">404</h1>
-      <h2 className="mt-4 text-2xl font-semibold">Page Not Found</h2>
-      <p className="mt-2 text-lg text-gray-600 max-w-lg">
-        The page you are looking for does not exist or has been moved.
+      <h2 className="text-5xl font-semibold text-primary">
+        Unauthorized access
+      </h2>
+      <p className="mt-5 text-lg text-gray-600 max-w-lg">
+        You do not have permission to access this page. Please contact your
+        administrator if you believe this is an error.
       </p>
       <Button
         className="mt-6"
