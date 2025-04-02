174f85091ad21214dc42039540f2c205fbc17168
Youssef Oubrik <yo.oubrik@gmail.com>
2025-04-02 16:57:16 +0000

    chore(types): update and refine type definitions

--- types/types.ts
+++ types/types.ts
@@ -9,44 +9,27 @@ export enum Role {
   ADMIN = "ADMIN",
 }
 
-export abstract class User {
+export interface BaseEntity {
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface User extends BaseEntity {
   id: string;
   email: string;
   role: Role;
-  joinedAt: Date;
-
-  constructor(id: string, email: string, role: Role, registredAt: Date) {
-    this.id = id;
-    this.email = email;
-    this.role = role;
-    this.joinedAt = registredAt;
-  }
 }
 
-export class Admin extends User {
+export interface Admin extends User {
   firstName: string;
   lastName: string;
   profilePicture: string;
-  constructor(id: string, email: string, firstName: string, lastName: string, profilePicture: string, joinedAt: Date) {
-    super(id, email, Role.ADMIN, joinedAt);
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.profilePicture = profilePicture;
-  }
 }
 
-export class Student extends User {
+export interface Student extends User {
   firstName: string;
   lastName: string;
   profilePicture: string;
   school?: string;
-  constructor(id: string, email: string, role = Role.STUDENT, firstName: string, lastName: string, profilePicture: string, school: string, joinedAt: Date) {
-    super(id, email, role, joinedAt);
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.profilePicture = profilePicture;
-    this.school = school;
-  }
 }
 
 export interface StudentToRemove {
@@ -54,10 +37,9 @@ export interface StudentToRemove {
   firstName: string;
   lastName: string;
 }
-export class Company extends User {
+export interface Company extends User {
   name: string;
   address: string;
-  createdAt: Date;
   applicationDate: Date;
   description: string;
   ice: string;
@@ -67,44 +49,10 @@ export class Company extends User {
   phone: string;
   size: string;
   socialLinks?: CompanySocialLinks;
-  updatedAt?: Date;
   website: string;
-  internships?: Internship[] = [];
+  internships?: Internship[];
 
-  constructor(
-    id: string,
-    email: string,
-    logo: string,
-    name: string,
-    address: string,
-    createdAt: Date,
-    description: string,
-    ice: string,
-    rc: string,
-    domain: string,
-    phone: string,
-    size: string,
-    updatedAt: Date,
-    website: string,
-    joinedAt: Date,
-    applicationDate: Date
-  ) {
-    super(id, email, Role.COMPANY, joinedAt);
-    this.name = name;
-    this.logo = logo;
-    this.address = address;
-    this.createdAt = createdAt;
-    this.description = description;
-    this.ice = ice;
-    this.rc = rc;
-    this.domain = domain;
-    this.logo = logo;
-    this.phone = phone;
-    this.size = size;
-    this.updatedAt = updatedAt;
-    this.website = website;
-    this.applicationDate = applicationDate;
-  }
+
 }
 
 export enum WorkMode {
@@ -125,7 +73,7 @@ export enum SalaryType {
   HOUR = "HOUR",
 }
 
-export class Internship {
+export interface Internship {
   id: string;
   company?: Company;
   companyId?: string;
@@ -145,48 +93,6 @@ export class Internship {
   renumerated: boolean;
   likes: string[];
   applicants: string[];
-
-  constructor(
-    id: string,
-    company: Company,
-    companyId: string,
-    createdAt: Date,
-    description: string,
-    duration: number,
-    salary: number,
-    salaryType: SalaryType,
-    domain: string,
-    location: string,
-    title: string,
-    updatedAt: Date,
-    workMode: WorkMode,
-    tags: InternshipType[],
-    skills: string[],
-    negotiable: boolean,
-    renumerated: boolean,
-    likes: string[] = [],
-    applicants: string[] = []
-  ) {
-    this.id = id;
-    this.company = company;
-    this.companyId = companyId;
-    this.createdAt = createdAt;
-    this.description = description;
-    this.duration = duration;
-    this.salary = salary;
-    this.salaryType = salaryType;
-    this.domain = domain;
-    this.location = location;
-    this.title = title;
-    this.updatedAt = updatedAt;
-    this.workMode = workMode;
-    this.tags = tags;
-    this.skills = skills;
-    this.negotiable = negotiable;
-    this.renumerated = renumerated;
-    this.likes = likes;
-    this.applicants = applicants;
-  }
 }
 
 export enum flagSeverity {
